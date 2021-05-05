0043a5672cf570ab50a5eb69c091a9c5bac98b10
Francisco Balboa <83018923+ftbalboa@users.noreply.github.com>
2021-05-05 15:23:40 -0300

    Add files via upload

---
+++ Utilidades/fastTest/fastTest.ino
@@ -0,0 +1,21 @@
+unsigned long mostrar = 0;
+unsigned long timer = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("INICIE");
+}
+
+void loop() { 
+
+ 
+   while (Serial.available()>0) 
+   {
+        char option = Serial.read();
+        Serial.println(option);
+   }
+
+  if(millis() > timer + 1000){timer = millis();Serial.println("A");}
+   
+}
