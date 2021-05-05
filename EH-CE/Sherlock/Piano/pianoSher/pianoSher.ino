3873a28761c51d12feca67c808a064bc5816ba29
Francisco Balboa <83018923+ftbalboa@users.noreply.github.com>
2021-05-05 15:45:23 -0300

    Add files via upload

---
+++ EH-CE/Sherlock/Piano/pianoSher/pianoSher.ino
@@ -0,0 +1,155 @@
+/* PIANO_06
+ * 
+ * MEGA, SENSORES DE RANURA Y RELE
+ * 
+ */
+ 
+//EDITABLE
+#define SPRINT true
+#define NEED_REPOSO false
+#define RELE  2
+#define A     3
+#define B     4
+#define C     5
+#define D     6
+#define E     7
+
+
+#define DS    10
+const int N_INPUTS = 5;
+const int INPUTS[N_INPUTS] = {A,B,C,D,E};
+const int INPUTS_ISTATE[N_INPUTS] = {1,1,1,1,1};
+const int N_OUTPUTS = 1;
+const int OUTPUTS[N_OUTPUTS] = {RELE};
+const int OUTPUTS_ISTATE[N_OUTPUTS] = {1};
+
+const int LARGO_SECUENCIA = /*16*/8;
+const int SECUENCIA[LARGO_SECUENCIA]={D,B,B,A,B,C,B,E};
+
+int lugar = 0;
+
+boolean ret_C1 = false;
+boolean ret_CS = false;
+boolean ret_D1 = false;
+boolean ret_E1 = false;
+boolean ret_F1 = false;
+boolean ret_G1 = false;
+boolean ret_A1 = false;
+boolean ret_B1 = false;
+boolean ret_C2 = false;
+
+//NO EDITABLE
+boolean reposo = false;
+boolean activo = false;
+boolean resuelto = false;
+unsigned long timer_print = 0;
+////////////////////////////////////SETUP///////////////////////////////
+//NO EDITABLE
+void setup() {
+  if(SPRINT){Serial.begin(9600);} 
+//declara entradas
+  for(int a = 0;a < N_INPUTS;a++){ 
+    pinMode(INPUTS[a] , INPUT);
+    digitalWrite(INPUTS[a] , INPUTS_ISTATE[a]);
+  }
+//declara salidas
+  for(int a = 0;a < N_OUTPUTS;a++){ 
+    pinMode(OUTPUTS[a] , OUTPUT);
+    digitalWrite(OUTPUTS[a] , OUTPUTS_ISTATE[a]);
+  }
+//si necesita reposo
+  if(NEED_REPOSO){reposo=true;activo=false;resuelto=false;}else{reposo=false;activo=true;resuelto=false;}
+//EDITABLE
+}
+
+///////////////////////////////////////LOOP//////////////////////////////////////////
+void loop() {
+  if_reposo();
+  if_activo();
+  if_resuelto();
+  showme();
+}
+
+////////////////////////////////////////FUNCIONES///////////////////////////////////
+//EDITABLE
+void if_reposo()
+{
+  if(reposo){
+  }
+}
+
+void if_activo()
+{
+  if(activo){
+
+    if(check(0, A ,DS)){
+      if(ret_C1){}else{ret_C1=true; if(A == SECUENCIA[lugar]){lugar++;}else{lugar = 0;}}
+    }else{
+      ret_C1=false;}
+    if(lugar == LARGO_SECUENCIA){lugar = 0; activo = false; resuelto = true;}
+    if(check(0, B ,DS)){
+      if(ret_CS){}else{ret_CS=true; if(B == SECUENCIA[lugar]){lugar++;}else{lugar = 0;}}
+    }else{
+      ret_CS=false;}
+    if(lugar == LARGO_SECUENCIA){lugar = 0; activo = false; resuelto = true;}
+    if(check(0, C ,DS)){
+      if(ret_D1){}else{ret_D1=true; if(C == SECUENCIA[lugar]){lugar++;}else{lugar = 0;}}
+    }else{
+      ret_D1=false;}
+    if(lugar == LARGO_SECUENCIA){lugar = 0; activo = false; resuelto = true;}
+    if(check(0, D ,DS)){
+      if(ret_E1){}else{ret_E1=true; if(D == SECUENCIA[lugar]){lugar++;}else{lugar = 0;}}
+    }else{
+      ret_E1=false;}
+    if(lugar == LARGO_SECUENCIA){lugar = 0; activo = false; resuelto = true;}
+    if(check(0, E ,DS)){
+      if(ret_F1){}else{ret_F1=true; if(E == SECUENCIA[lugar]){lugar++;}else{lugar = 0;}}
+    }else{
+      ret_F1=false;}
+    if(lugar == LARGO_SECUENCIA){lugar = 0; activo = false; resuelto = true;}
+
+  }
+}
+
+void if_resuelto()
+{
+  if(resuelto){
+    digitalWrite(RELE, LOW);
+    delay(5000);
+    digitalWrite(RELE, HIGH);
+    resuelto = false;
+    activo = true;
+  }
+}
+
+void showme ()
+{
+  if(SPRINT && millis() - timer_print > 1000)
+  {
+    Serial.println("------NUEVA MEDICION------");timer_print = millis();
+    Serial.print("A: ");
+    Serial.println(ret_C1);
+    Serial.print("B: ");
+    Serial.println(ret_CS);
+    Serial.print("C1: ");
+    Serial.println(ret_D1);
+    Serial.print("D1: ");
+    Serial.println(ret_E1);
+    Serial.print("E1: ");
+    Serial.println(ret_F1);
+
+    Serial.print("Lugar: ");
+    Serial.println(lugar);
+    }
+  }
+
+//NO EDITABLE
+//comprueba el estado de un sensor
+boolean check (boolean cual, int entrada, int ds)
+{
+  boolean resu = false;
+  int contador = 0;  
+  if(cual){ while(digitalRead(entrada) == 1 && resu == false){delay(ds);contador++;if(contador == 5){resu = true;}}
+  }else{    while(digitalRead(entrada) == 0 && resu == false){delay(ds);contador++;if(contador == 5){resu = true;}}}  
+  return resu; 
+}
---
+++ EH-CE/Sherlock/relojSher/relojSher.ino
@@ -0,0 +1,164 @@
+
+/* Reloj
+ * 
+ * Arduino Nano, 1 sensor de puerta, 1 boton de reset
+ * 
+ * 
+ * Sputnik Ar
+ * 21/08/19
+ */
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+//DEFINES
+#define IMAN  2 
+#define RESET 3
+
+#define DS  50
+  
+const int pinCS = 10;
+const int numberOfHorizontalDisplays = 4;
+const int numberOfVerticalDisplays = 2;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+const int wait = 100; // En milisegundos
+ 
+const int spacer = 1;
+const int width = 6 + spacer; // Ancho de la fuente a 5 pixeles
+
+//VARIABLES RELOJ
+unsigned long contador = 0;
+int HOR_FIN = 0;
+int SEG_FIN = 0;
+int MIN_FIN = 0;
+int SEG_RES = 0;
+int C_TIMER = 0;
+unsigned int tiempo_restante = 3600;
+unsigned long contador_tiempo = 0;
+char toString[5] = {'T','6','0','0','0'};
+boolean play = true;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IMAN,INPUT);
+  pinMode(RESET,INPUT);
+  pinMode(7,INPUT);
+  pinMode(6,INPUT);
+  pinMode(5,INPUT);
+  digitalWrite(IMAN,1);
+  digitalWrite(RESET,1);
+ matrix.setIntensity(7); // Ajustar el brillo entre 0 y 15
+ 
+ // Ajustar segun las necesidades
+ matrix.setPosition(0, 0, 0); // El primer display esta en <0, 0>
+ matrix.setPosition(1, 1, 0); // El segundo display esta en <1, 0>
+ matrix.setPosition(2, 2, 0); // El tercer display esta en <2, 0>
+ matrix.setPosition(3, 3, 0); // El cuarto display esta en <3, 0>
+ matrix.setPosition(4, 4, 0); // El quinto display esta en <4, 0>
+ matrix.setPosition(5, 5, 0); // El sexto display esta en <5, 0>
+ matrix.setPosition(6, 6, 0); // El séptimo display esta en <6, 0>
+ matrix.setPosition(7, 7, 0); // El octavo display esta en <7, 0>
+ matrix.setPosition(8, 8, 0); // El noveno display esta en <8, 0>
+ 
+ matrix.setRotation(0, 1); // Posición del display
+ matrix.setRotation(1, 1); // Posición del display
+ matrix.setRotation(2, 1); // Posición del display
+ matrix.setRotation(3, 1); // Posición del display
+ matrix.setRotation(4, 1); // Posición del display
+ matrix.setRotation(5, 1); // Posición del display
+ matrix.setRotation(6, 1); // Posición del display
+ matrix.setRotation(7, 1); // Posición del display
+ matrix.setRotation(8, 1); // Posición del display
+ relojea();
+}
+
+void loop() {
+// if(check(0,RESET,DS)){tiempo_restante = 3600;play=true;relojea();}
+ relojea();
+ controlea();
+
+
+}
+
+
+void relojea()
+{
+  if(play){
+    if ((millis() - contador_tiempo) >= 1000) {        //
+      contador_tiempo = millis();
+      Serial.println(tiempo_restante);
+      if(tiempo_restante != 0){
+        tiempo_restante = tiempo_restante - 1;
+        actualiza();
+        matrix.write();
+      }
+    }  
+  }
+}
+  
+
+
+void actualiza(){
+
+  SEG_RES = tiempo_restante; /*- segundos*/;
+
+  if(SEG_RES >= 0){
+    int probador = 0;
+    probador = SEG_RES / 3600;
+    HOR_FIN = 0;     
+    while(probador >= 1){HOR_FIN++;probador--;}    
+    probador = (SEG_RES - HOR_FIN * 3600) / 60;
+    MIN_FIN = 0;     
+    while(probador >= 1){MIN_FIN++;probador--;}
+    SEG_FIN = SEG_RES - HOR_FIN * 3600 - MIN_FIN * 60;   
+    int decenas = 0;
+    int unidades = 0;
+    
+    if(HOR_FIN > 0){     
+      decenas = HOR_FIN / 10;
+      unidades = HOR_FIN - decenas * 10;
+      matrix.drawChar(0, 0, decenas+48, HIGH, LOW, 1);
+      matrix.drawChar(6, 0, unidades+48, HIGH, LOW, 1);
+      matrix.drawChar(12, 0, ':', HIGH, LOW, 1);
+      decenas = MIN_FIN / 10;
+      unidades = MIN_FIN - decenas * 10;
+      matrix.drawChar(18, 0, decenas+48, HIGH, LOW, 1);
+      matrix.drawChar(24, 0, unidades+48, HIGH, LOW, 1);           
+      }else{       
+      decenas = MIN_FIN / 10;
+      unidades = MIN_FIN - decenas * 10;
+      matrix.drawChar(2, 4, decenas+48, HIGH, LOW, 1);
+      matrix.drawChar(8, 4, unidades+48, HIGH, LOW, 1);
+      matrix.drawChar(14, 4, ':', HIGH, LOW, 1);
+      decenas = SEG_FIN / 10;
+      unidades = SEG_FIN - decenas * 10;
+      matrix.drawChar(20, 4, decenas+48, HIGH, LOW, 1);
+      matrix.drawChar(26, 4, unidades+48, HIGH, LOW, 1);       
+      
+        }
+    
+    }
+  
+  }
+
+void controlea()
+{
+  if(check(1,7,DS)){
+    if(check(1,6,DS)){Serial.println('6');}
+    if(check(1,5,DS)){Serial.println('5');} 
+    Serial.println("Entre");
+    }
+  
+  
+  }
+
+boolean check (boolean cual, int entrada, int ds)
+{
+  boolean resu = false;
+  int contador = 0;  
+  if(cual){ while(digitalRead(entrada) == 1 && resu == false){contador++;if(contador == ds){resu = true;}}
+  }else{    while(digitalRead(entrada) == 0 && resu == false){contador++;if(contador == ds){resu = true;}}}  
+  return resu; 
+}
