eb5021c489f55dbaf3aeacb112b599893e010518
Francisco Balboa <83018923+ftbalboa@users.noreply.github.com>
2021-05-05 07:07:37 -0300

    Add files via upload

---
+++ JM/Canning/_base_devices.ino/_base_devices.ino.ino
@@ -0,0 +1,77 @@
+/* BASE CANNING
+ * 
+ * 
+ * 
+ */
+ 
+//EDITABLE
+#define SPRINT false
+#define NEED_REPOSO false
+const int N_INPUTS = 1;
+const int INPUTS[N_INPUTS] = {22};
+const int INPUTS_ISTATE[N_INPUTS] = {HIGH};
+const int N_OUTPUTS = 1;
+const int OUTPUTS[N_OUTPUTS] = {23};
+const int OUTPUTS_ISTATE[N_OUTPUTS] = {HIGH};
+
+//NO EDITABLE
+boolean reposo = false;
+boolean activo = false;
+boolean resuelto = false;
+
+////////////////////////////////////SETUP///////////////////////////////
+//NO EDITABLE
+void setup() {
+  if(SPRINT){Serial.begin(9600);} 
+//declara entradas
+  for(int a = 0;a < N_INPUTS;a++){ 
+    pinMode(INPUTS[a] , INPUT);
+    digitalWrite(INPUTS[a] , INPUTS_ISTATE[a]);
+  }
+//declara salidas
+  for(int a = 0;a < N_OUTPUTS;a++){ 
+    pinMode(OUTPUTS[a] , OUTPUT);
+    digitalWrite(OUTPUTS[a] , OUTPUTS_ISTATE[a]);
+  }
+//si necesita reposo
+  if(NEED_REPOSO){reposo=true;activo=false;resuelto=false;}else{reposo=false;activo=true;resuelto=false;}
+//EDITABLE
+}
+
+///////////////////////////////////////LOOP//////////////////////////////////////////
+void loop() {
+  if_reposo();
+  if_activo();
+  if_resuelto();
+}
+
+////////////////////////////////////////FUNCIONES///////////////////////////////////
+//EDITABLE
+void if_reposo()
+{
+  if(reposo){
+  }
+}
+
+void if_activo()
+{
+  if(activo){
+  }
+}
+
+void if_resuelto()
+{
+  if(resuelto){
+  }
+}
+
+//NO EDITABLE
+//comprueba el estado de un sensor
+boolean check (boolean cual, int entrada, int ds)
+{
+  boolean resu = false;
+  int contador = 0;  
+  if(cual){ while(digitalRead(entrada) == 1 && resu == false){delay(ds);contador++;if(contador == 5){resu = true;}}
+  }else{    while(digitalRead(entrada) == 0 && resu == false){delay(ds);contador++;if(contador == 5){resu = true;}}}  
+  return resu; 
+}
